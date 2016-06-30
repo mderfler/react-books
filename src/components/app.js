c5225f497935df4d5a0b870fff26804bc24a569c
unknown <mderfler@LT10372.catalystsolves.com>
2016-06-29 21:05:41 -0700

    switched to pattern from tutorial; began searchbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,71 @@
 import React, {Component} from 'react';
 
 import Booklist from './booklist';
+import SearchBar from './searchbar';
 import getBooks from '../services/index.jsx';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: []
-        };
-        this.findBooks();
-}
 
- findBooks() {
+var App = React.createClass({
+  getInitialState: function() {
+    return {books: []};
+  },
+  componentDidMount: function() {
+    this.fetchBooks();
+    
+  },
+  fetchBooks: function(){
     var array = [];
     getBooks.getBooks().then(function(data){
                 
                     for (var i = data.length - 1; i >= 0; i--) {
                         array.push(data[i])
                     }
-                })
-    this.state.books.push(array);
-}
+                }).then(
+                    this.setState({books: array})
+                )
+  },
+   bookSearch: function(term){
+    console.log(term)
+  },
 
-    render() {
-        return (
-            <div>
-             Hello from App in component folder
-            <Booklist books={this.state.books[0]} />
-            </div>
-        );
-    }
-}
+  render: function() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={this.bookSearch}/>
+        <Booklist books={this.state.books} />
+      </div>
+          );
+  }
+})
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             books: []
+//         };
+//         this.findBooks();
+// }
+
+//  findBooks() {
+//     var array = [];
+//     getBooks.getBooks().then(function(data){
+                
+//                     for (var i = data.length - 1; i >= 0; i--) {
+//                         array.push(data[i])
+//                     }
+//                 })
+//     this.state.books.push(array);
+// }
+
+//     render() {
+//         return (
+//             <div>
+//              Hello from App in component folder
+//             <Booklist books={this.state.books[0]} />
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
--- src/components/booklist.js
+++ src/components/booklist.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import getBooks from '../services/index.jsx';
+import Author from './author';
 
 const Booklist = ({books})=>{
 
@@ -9,15 +10,30 @@ const Booklist = ({books})=>{
  
   const booklist = books.map((book) => {
     return (
-      <div key={book.id}>{book.title}</div>
+    <div>
+      <li key={book.id}>
+        {book.title}
+      <div key={book.id}>
+        genre: {book.genre}
+      </div>
+      <div key={Date.now()}>
+      <Author author={book.author} />
+      </div>
+      <div>
+        year: {book.publication}
+      </div>
+      </li>
+    </div>
     );
   });
 
 
 return (
+<div>
     <ul className="col-md-4 list-group">
       {booklist}
     </ul>
+</div>
   );
 
 }
